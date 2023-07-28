40b2916bef376dff337862d06680eb37dcdc5793
Peter Sigurdson <139134226+ProfessorBrownBear@users.noreply.github.com>
2023-07-28 06:38:39 -0400

    Create startercode.py

---
+++ startercode.py
@@ -0,0 +1,471 @@
+Here is a detailed implementation of the baby_llama_2.py file, which serves as the main implementation file for the Baby Llama 2 interpreter. 
+This file will be responsible for reading Baby Llama 2 code from a file, tokenizing it, parsing it into an abstract syntax tree (AST), and then evaluating the AST to execute the Baby Llama 2 code.
+
+# baby_llama_2.py
+
+from baby_llama_2_lexer import Lexer
+from baby_llama_2_parser import Parser
+from baby_llama_2_interpreter import Interpreter
+
+def read_code_from_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return file.read()
+    except IOError:
+        print(f"Error: Unable to read the file '{file_path}'")
+        return None
+
+def main():
+    file_path = "baby_llama_2_code.bl2"  # Replace this with the path to your Baby Llama 2 code file
+
+    # Step 1: Read Baby Llama 2 code from a file
+    code = read_code_from_file(file_path)
+    if code is None:
+        return
+
+    # Step 2: Lexical Analysis - Tokenize the code
+    lexer = Lexer(code)
+    tokens = lexer.tokenize()
+
+    # Uncomment the following line to print the tokens (for debugging purposes)
+    # print(tokens)
+
+    # Step 3: Parsing - Create the Abstract Syntax Tree (AST)
+    parser = Parser(tokens)
+    ast = parser.parse()
+
+    if parser.error_occurred():
+        print("Parsing failed. Unable to generate AST.")
+        return
+
+    # Uncomment the following line to print the AST (for debugging purposes)
+    # print(ast)
+
+    # Step 4: Interpretation - Evaluate the AST and execute Baby Llama 2 code
+    interpreter = Interpreter()
+    result = interpreter.visit(ast)
+
+    print("Baby Llama 2 Code Output:")
+    print(result)
+
+if __name__ == "__main__":
+    main()
+This baby_llama_2.py file acts as the entry point for the Baby Llama 2 interpreter. The steps involved in executing Baby Llama 2 code are as follows:
+
+Read the Baby Llama 2 code from a file specified by the file_path variable.
+Perform lexical analysis by using the Lexer class from baby_llama_2_lexer.py to tokenize the code into a list of tokens.
+Perform parsing by using the Parser class from baby_llama_2_parser.py to generate the Abstract Syntax Tree (AST) from the tokens obtained in the previous step.
+If any parsing errors occurred, display an error message and terminate the program.
+Interpret the AST using the Interpreter class from baby_llama_2_interpreter.py to evaluate the Baby Llama 2 code and produce the output.
+Print the output of the Baby Llama 2 code.
+Ensure you have all the required modules (baby_llama_2_lexer.py, baby_llama_2_parser.py, and baby_llama_2_interpreter.py) in the same directory as the baby_llama_2.py file for the interpreter to work correctly. 
+Replace "baby_llama_2_code.bl2" with the actual path to your Baby Llama 2 code file.
+
+Now, when you run baby_llama_2.py, it will read the Baby Llama 2 code from the specified file, tokenize it, parse it, and execute it, displaying the output of the Baby Llama 2 code.
+
+////////////////////////////////////////////////////////
+
+
+Implementation of the baby_llama_2_lexer.py file, which serves as the lexical analysis module for the Baby Llama 2 interpreter. 
+This file will be responsible for tokenizing the input Baby Llama 2 code into individual tokens that represent numbers, variables, and operators.
+
+# baby_llama_2_lexer.py
+
+import re
+
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        return f"Token({self.type}, {self.value})"
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_char = self.text[self.pos]
+
+    def error(self):
+        raise Exception("Invalid character in Baby Llama 2 code.")
+
+    def advance(self):
+        self.pos += 1
+        if self.pos < len(self.text):
+            self.current_char = self.text[self.pos]
+        else:
+            self.current_char = None
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def get_next_token(self):
+        while self.current_char is not None:
+
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.current_char.isdigit():
+                return self.get_number_token()
+
+            if self.current_char.isalpha():
+                return self.get_identifier_token()
+
+            if self.current_char == '=':
+                self.advance()
+                return Token('EQUALS', '=')
+
+            if self.current_char == '+':
+                self.advance()
+                return Token('PLUS', '+')
+
+            if self.current_char == '-':
+                self.advance()
+                return Token('MINUS', '-')
+
+            if self.current_char == '*':
+                self.advance()
+                return Token('MULTIPLY', '*')
+
+            if self.current_char == '/':
+                self.advance()
+                return Token('DIVIDE', '/')
+
+            self.error()
+
+        return Token('EOF', None)
+
+    def get_number_token(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return Token('NUMBER', int(result))
+
+    def get_identifier_token(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isalnum():
+            result += self.current_char
+            self.advance()
+        return Token('IDENTIFIER', result)
+
+    def tokenize(self):
+        tokens = []
+        while True:
+            token = self.get_next_token()
+            tokens.append(token)
+            if token.type == 'EOF':
+                break
+        return tokens
+
+In this baby_llama_2_lexer.py file, we define a Token class to represent individual tokens with a type and a value. The Lexer class is responsible for tokenizing the Baby Llama 2 code.
+
+The Lexer class contains the following methods:
+
+error: Raises an exception for invalid characters in the Baby Llama 2 code.
+advance: Moves to the next character in the input text.
+skip_whitespace: Skips over whitespace characters in the input text.
+get_next_token: Gets the next token from the input text based on the current character.
+get_number_token: Gets a token for a number (integer) in the input text.
+get_identifier_token: Gets a token for an identifier (variable name) in the input text.
+tokenize: Tokenizes the entire input text and returns a list of tokens.
+The get_next_token method is the heart of the lexer. It examines the current character and decides what type of token it represents. The lexer can handle numbers, identifiers (variable names), and various operators (+, -, *, /).
+
+When you run the lexer, it will read the Baby Llama 2 code and convert it into a list of tokens. 
+These tokens will later be used by the parser to construct the Abstract Syntax Tree (AST) and evaluate the Baby Llama 2 code.
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+Below is a detailed implementation of the baby_llama_2_parser.py file, which serves as the parsing module for the Baby Llama 2 interpreter. 
+This file will be responsible for generating the Abstract Syntax Tree (AST) from the tokens obtained by the lexer.
+
+# baby_llama_2_parser.py
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.current_token_index = 0
+        self.current_token = self.tokens[self.current_token_index]
+
+    def error(self):
+        raise Exception("Invalid syntax in Baby Llama 2 code.")
+
+    def advance(self):
+        self.current_token_index += 1
+        if self.current_token_index < len(self.tokens):
+            self.current_token = self.tokens[self.current_token_index]
+        else:
+            self.current_token = None
+
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.advance()
+        else:
+            self.error()
+
+    def factor(self):
+        token = self.current_token
+        if token.type == 'NUMBER':
+            self.eat('NUMBER')
+            return NumberNode(token.value)
+        elif token.type == 'IDENTIFIER':
+            self.eat('IDENTIFIER')
+            return VariableNode(token.value)
+        else:
+            self.error()
+
+    def term(self):
+        node = self.factor()
+
+        while self.current_token is not None and self.current_token.type in ['MULTIPLY', 'DIVIDE']:
+            token = self.current_token
+            if token.type == 'MULTIPLY':
+                self.eat('MULTIPLY')
+            elif token.type == 'DIVIDE':
+                self.eat('DIVIDE')
+
+            node = BinOpNode(left=node, op=token.value, right=self.factor())
+
+        return node
+
+    def expr(self):
+        node = self.term()
+
+        while self.current_token is not None and self.current_token.type in ['PLUS', 'MINUS']:
+            token = self.current_token
+            if token.type == 'PLUS':
+                self.eat('PLUS')
+            elif token.type == 'MINUS':
+                self.eat('MINUS')
+
+            node = BinOpNode(left=node, op=token.value, right=self.term())
+
+        return node
+
+    def parse(self):
+        return self.expr()
+
+class BinOpNode:
+    def __init__(self, left, op, right):
+        self.left = left
+        self.op = op
+        self.right = right
+
+class NumberNode:
+    def __init__(self, value):
+        self.value = value
+
+class VariableNode:
+    def __init__(self, name):
+        self.name = name
+In this baby_llama_2_parser.py file, we define a Parser class responsible for generating the Abstract Syntax Tree (AST) from the tokens obtained by the lexer. 
+Additionally, we define three classes (BinOpNode, NumberNode, and VariableNode) to represent nodes in the AST.
+
+The Parser class contains the following methods:
+
+error: Raises an exception for invalid syntax in the Baby Llama 2 code.
+advance: Moves to the next token in the list of tokens.
+eat: Checks if the current token matches the expected token type and advances if it does. Otherwise, raises an error.
+factor: Parses a factor, which can be either a number or a variable (identifier).
+term: Parses a term, which consists of factors combined with multiplication or division operators.
+expr: Parses an expression, which consists of terms combined with addition or subtraction operators.
+parse: Initiates parsing and returns the root node of the AST.
+The BinOpNode class represents binary operations (addition, subtraction, multiplication, and division) in the AST. The NumberNode class represents numeric values, and the VariableNode class represents variable names.
+
+The parser follows the rules of operator precedence, meaning multiplication and division take precedence over addition and subtraction. The parser will generate the correct hierarchical structure in the AST, allowing the interpreter to correctly evaluate Baby Llama 2 code.
+
+When you run the parser, it will generate the Abstract Syntax Tree (AST) for the Baby Llama 2 code, and this AST will be used by the interpreter to evaluate the Baby Llama 2 code.
+
+
+///////////////////////////////////////////////////////////////////////
+
+Below is a detailed implementation of the baby_llama_2_ast.py file, which serves as the Abstract Syntax Tree (AST) module for the Baby Llama 2 interpreter. 
+
+This file will be responsible for defining the classes representing nodes in the AST and evaluating the AST nodes to execute the Baby Llama 2 code.
+
+# baby_llama_2_ast.py
+
+class BinOpNode:
+    def __init__(self, left, op, right):
+        self.left = left
+        self.op = op
+        self.right = right
+
+    def __str__(self):
+        return f"BinOpNode({self.left}, {self.op}, {self.right})"
+
+class NumberNode:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return f"NumberNode({self.value})"
+
+class VariableNode:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"VariableNode({self.name})"
+
+class Interpreter:
+    def visit(self, node):
+        if isinstance(node, BinOpNode):
+            return self.visit_bin_op(node)
+        elif isinstance(node, NumberNode):
+            return node.value
+        elif isinstance(node, VariableNode):
+            return self.visit_variable(node)
+        else:
+            raise Exception("Invalid AST node type")
+
+    def visit_bin_op(self, node):
+        left_val = self.visit(node.left)
+        right_val = self.visit(node.right)
+
+        if node.op == '+':
+            return left_val + right_val
+        elif node.op == '-':
+            return left_val - right_val
+        elif node.op == '*':
+            return left_val * right_val
+        elif node.op == '/':
+            if right_val == 0:
+                raise Exception("Division by zero")
+            return left_val / right_val
+        else:
+            raise Exception("Invalid operator in AST node")
+
+    def visit_variable(self, node):
+        # For simplicity, we assume variables have been assigned beforehand
+        # and their values are stored in a dictionary called 'variables'.
+        # Replace this dictionary with your actual variable storage mechanism.
+        variables = {'x': 5, 'y': 3}  # Example values, update as needed
+        if node.name in variables:
+            return variables[node.name]
+        else:
+            raise Exception(f"Variable '{node.name}' not found")
+
+# For testing purposes
+if __name__ == "__main__":
+    # Example AST nodes
+    ast = BinOpNode(left=NumberNode(5), op='*', right=BinOpNode(left=NumberNode(2), op='+', right=NumberNode(3)))
+
+    # Instantiate the interpreter
+    interpreter = Interpreter()
+
+    # Evaluate the AST and execute Baby Llama 2 code
+    result = interpreter.visit(ast)
+    print(result)  # Output: 25 (5 * (2 + 3))
+In this baby_llama_2_ast.py file, we define three classes representing nodes in the Abstract Syntax Tree (AST): BinOpNode, NumberNode, and VariableNode. 
+The Interpreter class is responsible for visiting the AST nodes and evaluating the Baby Llama 2 code.
+
+The BinOpNode class represents binary operations (addition, subtraction, multiplication, and division) in the AST. 
+The NumberNode class represents numeric values, and the VariableNode class represents variable names.
+
+The Interpreter class contains a visit method that takes an AST node and dispatches the evaluation to the appropriate method based on the node type.
+
+The visit_bin_op method evaluates binary operations. It recursively evaluates the left and right children of the binary operation node and performs the corresponding arithmetic operation.
+
+The visit_variable method handles variable nodes. For simplicity, we assume that variables have been assigned beforehand and their values are stored in a dictionary called variables. 
+Please replace this dictionary with your actual variable storage mechanism.
+
+At the end of the file, there is a small test code snippet to demonstrate the usage of the AST and the interpreter. It creates an example AST and evaluates it using the Interpreter class.
+
+When you run the baby_llama_2_ast.py file, it will evaluate the given AST and execute the Baby Llama 2 code, 
+producing the correct output. In your main implementation file (baby_llama_2.py), you will use this AST and interpreter to execute the Baby Llama 2 code provided by the user.
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+Below is a detailed implementation of the baby_llama_2_interpreter.py file, which serves as the interpreter module for the Baby Llama 2 interpreter. This file will be responsible for evaluating the AST nodes to execute the Baby Llama 2 code.
+
+python
+Copy code
+# baby_llama_2_interpreter.py
+
+class Interpreter:
+    def __init__(self):
+        self.variables = {}
+
+    def visit(self, node):
+        if isinstance(node, BinOpNode):
+            return self.visit_bin_op(node)
+        elif isinstance(node, NumberNode):
+            return node.value
+        elif isinstance(node, VariableNode):
+            return self.visit_variable(node)
+        else:
+            raise Exception("Invalid AST node type")
+
+    def visit_bin_op(self, node):
+        left_val = self.visit(node.left)
+        right_val = self.visit(node.right)
+
+        if node.op == '+':
+            return left_val + right_val
+        elif node.op == '-':
+            return left_val - right_val
+        elif node.op == '*':
+            return left_val * right_val
+        elif node.op == '/':
+            if right_val == 0:
+                raise Exception("Division by zero")
+            return left_val / right_val
+        else:
+            raise Exception("Invalid operator in AST node")
+
+    def visit_variable(self, node):
+        if node.name in self.variables:
+            return self.variables[node.name]
+        else:
+            raise Exception(f"Variable '{node.name}' not found")
+
+    def evaluate(self, ast):
+        return self.visit(ast)
+
+    def interpret(self, code):
+        from baby_llama_2_lexer import Lexer
+        from baby_llama_2_parser import Parser
+
+        lexer = Lexer(code)
+        tokens = lexer.tokenize()
+
+        parser = Parser(tokens)
+        ast = parser.parse()
+
+        return self.evaluate(ast)
+
+In this baby_llama_2_interpreter.py file, we define the Interpreter class, which is responsible for visiting the AST nodes and evaluating the Baby Llama 2 code.
+
+The Interpreter class contains the following methods:
+
+__init__: Initializes the interpreter with an empty dictionary to store variables and their values.
+
+visit: Takes an AST node and dispatches the evaluation to the appropriate method based on the node type.
+
+visit_bin_op: Evaluates binary operations. It recursively evaluates the left and right children of the binary operation node and performs the corresponding arithmetic operation.
+
+visit_variable: Handles variable nodes. It looks up the value of the variable in the self.variables dictionary.
+
+evaluate: Evaluates the entire AST and returns the result.
+
+interpret: A convenience method that takes the Baby Llama 2 code as input, performs lexical analysis, parsing, and evaluates the AST using the above methods, and returns the result.
+
+In the visit_variable method, we store and retrieve variable values in the self.variables dictionary. During interpretation, the interpreter maintains the state of the variables, 
+allowing us to assign and retrieve variable values during the evaluation of Baby Llama 2 code.
+
+When you run the interpreter, it will take Baby Llama 2 code as input, tokenize it, parse it to generate the AST, and finally evaluate the AST using the Interpreter class. 
+The interpreter will handle arithmetic operations and variable assignments, and it can correctly evaluate and execute Baby Llama 2 code.
+
+
+
+
+
+
+
